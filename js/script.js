9295f63207e2e4cfa8789abfb47d3952af388724
Edo <edomeciar@gmail.com>
2015-08-25 23:30:41 +0200

    Define basic structure of js, map pois with knockout to simple div

--- js/script.js
+++ js/script.js
@@ -1,34 +1,59 @@
 $(function(){
-	ViewModel.init();
+	var mapModel = new MapModel();
 });
 
 /*model*/
-var MapModel = {
-	defaultValues:{
-		center: {lat: 48.549429090642946, lng: 19.65522705078128},
-    	zoom: 8
-	},
-	map:{},
+var MapModel = function(){
+	"use strict";
+	var pois,
+		defaultValues={
+			center: {lat: 48.549429090642946, lng: 19.65522705078128},
+    		zoom: 8
+		},
+		map;
 
-	init:function(){
+	function init(){
+		pois = [];
+		map = new google.maps.Map(document.getElementById('map'), defaultValues);
+		ko.applyBindings(new PoisViewModel());
+	}
+
+	function poi(){
 
 	}
 
+	function PoisViewModel(){
+		var self = this;
 
+		self.selected = ko.observable();
 
-};
+		self.allPois=[
+			{
+				name:"Lomnický peak",
+				location:{
+					lat:49.195272,
+					lng:20.213147
+				},
+				wiki:"Lomnický peak"
+			},
+			{
+				name:"Bojnice Castle",
+				location:{
+					lat:48.780281,
+					lng:18.577452
+				},
+				wiki:"Bojnice Castle"
+			}
+		];
 
-var TitleViewModel = function(){
-	this.projectTitle = "Beuaty of Slovakia!";
-}
-
-/*view model*/
-var ViewModel = {
-
-	init: function(){
-		MapModel.init();
-		ko.applyBindings(new TitleViewModel());
-		MapModel.map = new google.maps.Map(document.getElementById('map'), MapModel.defaultValues);
+		self.pois = ko.observableArray(self.allPois);
 	}
+
+	init();
+
 };
+
+
+
+
 

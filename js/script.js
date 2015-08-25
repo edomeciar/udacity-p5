866f61a1480524576d32d4b05f374b7b7771d908
Edo <edomeciar@gmail.com>
2015-08-26 00:17:15 +0200

    add detail about pois

--- js/script.js
+++ js/script.js
@@ -5,21 +5,41 @@ $(function(){
 /*model*/
 var MapModel = function(){
 	"use strict";
-	var pois,
-		defaultValues={
+	var defaultValues={
 			center: {lat: 48.549429090642946, lng: 19.65522705078128},
     		zoom: 8
 		},
-		map;
+		map,
+		panorama,
+		labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+		labelIndex = 0
 
-	function init(){
-		pois = [];
-		map = new google.maps.Map(document.getElementById('map'), defaultValues);
+	function initialize(){
+		map = new google.maps.Map(document.getElementById('map-canvas'), defaultValues);
 		ko.applyBindings(new PoisViewModel());
+
+		/*
+		panorama = new google.maps.StreetViewPanorama(
+      		document.getElementById('street-view'),
+      		{
+        		position: {lat: 37.869260, lng: -122.254811},
+        		pov: {heading: 165, pitch: 0},
+        		zoom: 1
+      		});
+      	*/
+
 	}
 
-	function poi(){
-
+	function poi(pName,pLocation){
+		var self = this;
+		self.label = labels[labelIndex++ % labels.length]
+		self.name = pName;
+		self.location = pLocation;
+		self.mapMarker = new google.maps.Marker({
+			position: new google.maps.LatLng(self.location.lat,self.location.lng),
+			label: self.label,
+			map: map
+		})
 	}
 
 	function PoisViewModel(){
@@ -27,29 +47,29 @@ var MapModel = function(){
 
 		self.selected = ko.observable();
 
-		self.allPois=[
+		self.defPois=[
 			{
 				name:"Lomnický peak",
 				location:{
 					lat:49.195272,
 					lng:20.213147
-				},
-				wiki:"Lomnický peak"
+				}
 			},
 			{
 				name:"Bojnice Castle",
 				location:{
 					lat:48.780281,
 					lng:18.577452
-				},
-				wiki:"Bojnice Castle"
+				}
 			}
 		];
 
-		self.pois = ko.observableArray(self.allPois);
+		var mappedPois = $.map(self.defPois, function(obj){ return new poi(obj.name, obj.location);});
+
+		self.pois = ko.observableArray(mappedPois);
 	}
 
-	init();
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 };
 
